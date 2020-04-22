0343a8f5feb60de29b9912f00eb5eb0076827a0b
Alex <mkumum12@mail.ru>
2020-04-22 12:34:32 +0300

    Installing and additing styles with interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'TourOfHeroes1111111111111';
 }
