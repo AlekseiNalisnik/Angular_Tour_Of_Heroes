8e95411990259b8edd27669a05165df5c1c83e4a
kupns <kupns@mail.ru>
2020-04-22 14:39:38 +0300

    append kupa-shop-api project and extend gitignore

---
+++ main/Kupreenkov_Nikita/ShopApi/Models/ShopItem.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShopApi
+{
+    public class ShopItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
