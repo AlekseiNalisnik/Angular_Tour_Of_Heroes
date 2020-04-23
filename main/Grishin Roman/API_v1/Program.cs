6bdd7389c6a05a15d0b31b603919f001f5b82772
grroma <mr.grischinroman@yandex.ru>
2020-04-23 17:13:15 +0300

    Story 1: Project Update

--- main/Grishin Roman/API_v1/Program.cs
+++ main/Grishin Roman/API_v1/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace TodoApi
+namespace API_v1
 {
     public class Program
     {
